2d28a09dc5413b67240aec64d404e0d62eb44184
Masaki Ihara <mxxxxi.ixxxxa@gmail.com>
2016-11-02 00:01:30 +0200

    Initial Commit
    Added main class and option class

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Dos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Option.readContents();
+        Option.showOptions();
+        Option.getOption();
+
+    }
+}
---
+++ Option.java
@@ -0,0 +1,40 @@
+package Dos;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by masaki on 11/1/2016.
+ */
+public class Option {
+    // Dictionary of options
+    static HashMap <Integer, String> options = new HashMap<>();
+
+    // Filling dictionary
+    public static void readContents(){
+        options.put(0, "0: Packet Attack");
+        options.put(1, "1: ICMP Attack");
+    }
+
+    // Show all the options available
+    public static void showOptions() {
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(options.get(i));
+        }
+    }
+
+    // Let user select an attack method
+    public static String getOption(){
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Select an attack method(enter a number): ");
+        int userChoice = Integer.parseInt(in.nextLine());
+
+        System.out.println("OK, you have selected " + options.get(userChoice));
+
+        String attackMethod = options.get(userChoice);
+
+        return attackMethod;
+    }
+
+}
